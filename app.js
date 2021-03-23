351ef06e69163a5d3003debe1fc30c39748ed552
Aniket Sharma <aniketsharma312@gmail.com>
2021-03-22 18:32:45 -0600

    up to date comit mar/22

---
+++ app.js
@@ -0,0 +1,9 @@
+new fullpage('#fullpage', {
+	//options here
+  anchors: ['section-1', 'section-2', 'section-3'],
+	autoScrolling:true,
+	scrollHorizontally: true
+});
+
+//methods
+fullpage_api.setAllowScrolling(false);
