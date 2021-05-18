cd2e67a10abcccfb1cf288fd55194624aaa7bc17
AniketS312 <aniketsharma312@gmail.com>
2021-05-18 09:21:19 -0600

    mobile optomization complete. Will go over once more.

--- app.js
+++ app.js
@@ -45,3 +45,20 @@ submitButton.addEventListener("click", function (event) {
   event.preventDefault();
   submitButton.value = "Thank you!";
 });
+
+// Close mobile menu
+const mobileItems = document.querySelectorAll("#item");
+const mobileMenu = document.getElementById("mobile-menu");
+
+mobileItems.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    mobileMenu.style.top = "-100%";
+  });
+});
+
+// Open mobile menu - borrowed mobileMenu from above
+const openButton = document.getElementById("open-menu");
+openButton.addEventListener("click", function () {
+  mobileMenu.style.top = "0";
+  console.log("click");
+});
